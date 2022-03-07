b7815d0b5dbced7d6e192b0f92907bb19a2f48c6
Gokul Menon <ohyesgocool@yahoo.com>
2022-03-07 12:48:45 +0100

    PalindromeNumber

---
+++ src/main/java/arrays/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package arrays;
+
+//https://leetcode.com/problems/palindrome-number/
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int sum = 0;
+        while (x > sum) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == sum || x == sum / 10);
+
+    }
+}
